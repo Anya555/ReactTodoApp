ba04bf9875584dea9ec0ed3caf49192d98a3d6be
Anna Panas <annapanas0906@gmail.com>
2020-05-26 17:07:25 -0400

    made changes to config

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,5 @@
 import app from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import "firebase/storage";
-
-var config = {
-  apiKey: "AIzaSyALFMidi6um1KqilX1FEjxUCQhz13BnP6c",
-  authDomain: "todolist-4b3b2.firebaseapp.com",
-  databaseURL: "https://todolist-4b3b2.firebaseio.com",
-  projectId: "todolist-4b3b2",
-  storageBucket: "todolist-4b3b2.appspot.com",
-  messagingSenderId: "640372971853",
-  appId: "1:640372971853:web:2408dfe7fbc958ce4f3f04",
-  measurementId: "G-8RHJF6MCHS",
-};
+import { config } from "./firebaseConfig";
 
 class Firebase {
   constructor() {
@@ -49,7 +36,6 @@ class Firebase {
   }
 
   getCurrentUsername() {
-    // console.log(this.auth.currentUser.uid);
     return this.auth.currentUser && this.auth.currentUser.displayName;
   }
 
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+export const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "todolist-4b3b2.firebaseapp.com",
+  databaseURL: "https://todolist-4b3b2.firebaseio.com",
+  projectId: "todolist-4b3b2",
+  storageBucket: "todolist-4b3b2.appspot.com",
+  messagingSenderId: "640372971853",
+  appId: "1:640372971853:web:2408dfe7fbc958ce4f3f04",
+  measurementId: "G-8RHJF6MCHS",
+};
