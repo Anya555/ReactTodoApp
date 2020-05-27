3e86bcfe66c2cc7d13092d7c833f39b8af815715
Anna Panas <annapanas0906@gmail.com>
2020-05-26 22:19:06 -0400

    env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,11 +4,11 @@ import "firebase/storage";
 
 export const config = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "todolist-4b3b2.firebaseapp.com",
-  databaseURL: "https://todolist-4b3b2.firebaseio.com",
-  projectId: "todolist-4b3b2",
-  storageBucket: "todolist-4b3b2.appspot.com",
-  messagingSenderId: "640372971853",
-  appId: "1:640372971853:web:2408dfe7fbc958ce4f3f04",
-  measurementId: "G-8RHJF6MCHS",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
